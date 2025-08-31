c32a7d6d9e14aa703b042aaa929c3cd3941d3a1f
Tasnim Borhan <www.borhantasnim08@gmail.com>
2025-08-31 21:34:39 +0600

    slicker

--- src/components/layouts/pages/Categories.jsx
+++ src/components/layouts/pages/Categories.jsx
@@ -7,43 +7,73 @@ import  console from "/src/assets/console.png"
 import  phone from "/src/assets/phone.png"
 import  headphone from "/src/assets/headphone.png"
 import  watch from "/src/assets/watch.png"
-
+// ===========================================
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 
 
 const Categories = () => {
+    var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+  };
   return (
-
- 
-   <>
-   <div className="py-10 bg-white">
-   <Container>
-    <Flex className="gap-x-5">
+   
+  <>
+  <Container>
+     <div className="py-10 bg-white">
+         <Flex className="gap-x-5">
      <div className="py-5 px-2 bg-[#DB4444] rounded-[5px]"></div>
      <div className="text-[#D44444]  ">Categories</div>
     </Flex>
-   </Container>
-   </div>
-   <div className="py-5">
-    <Container>
-        <h3 className="text-3xl font-semibold">Browse By Category</h3>
-    </Container>
-   </div>
-   <div className="py-10 bg-white">
-    <Container>
-        <Flex className="gap-x-20">
-            <Image imgSrc={computer} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
-           
-            <Image imgSrc={console} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
-             <Image imgSrc={watch} className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
-              <Image imgSrc={headphone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] "/>      
-               <Image imgSrc={computer}className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
-                <Image imgSrc={phone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
-        </Flex>
-    </Container>
-   </div>
+     </div>
+      <div className="py-5"></div>
+       <h3 className="text-3xl font-semibold">Browse By Category</h3>
+     <Slider {...settings} className="py-20">
+      <div>
+       <Image imgSrc={computer} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
+      </div>
+      <div>
+        <Image imgSrc={console} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
+      </div>
+      <div>
+       <Image imgSrc={watch} className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
+      </div>
+      <div>
+         <Image imgSrc={headphone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] "/> 
+      </div>
+      <div>
+       <Image imgSrc={phone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
+      </div>
+      <div>
+       <Image imgSrc={computer} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
+      </div>
+      <div>
+        <Image imgSrc={console} className="hover:bg-[#DB4444] duration-500 rounded-[5px]"/>
+      </div>
+      <div>
+       <Image imgSrc={watch} className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
+      </div>
+      <div>
+         <Image imgSrc={headphone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] "/> 
+      </div>
+      <div>
+       <Image imgSrc={phone}className="hover:bg-[#DB4444] duration-500 rounded-[5px] " />
+      </div>
+     
+    </Slider>
+  </Container>
+
+  
+   
    </>
   )
 }
+
 
 export default Categories
